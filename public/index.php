580aa685442ddd276482a8f0d1ebc3d1d400ea23
elias <eliasejump@gmail.com>
2023-01-15 17:19:03 +0300

    set base conf

--- public/index.php
+++ public/index.php
@@ -10,12 +10,12 @@ echo "Привет, Хозяин!<br>" . date("Y-m-d H:i:s") . "<br><br>";
 
 include_once("../src/TryService.php");
 
-tryService('Redis', fn() => (new \Redis)->connect('redis'));
-tryService('Memcache', fn() => (new \Memcached)->addServer("memcached", 11211));
-tryService('MySQL', function () {
-    $connect = mysqli_connect('db', 'root', 'root');
-    mysqli_close($connect);
-});
+//tryService('Redis', fn() => (new \Redis)->connect('redis'));
+//tryService('Memcache', fn() => (new \Memcached)->addServer("memcached", 11211));
+//tryService('MySQL', function () {
+//    $connect = mysqli_connect('db', 'root', 'root');
+//    mysqli_close($connect);
+//});
 
 phpinfo(); ?>
 </body>
